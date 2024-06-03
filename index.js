eef9e9988da2fb0b338f0cdc1b7e0a10f394b3fc
lee <lsljw65@gmail.com>
2024-06-03 17:29:30 +0900

    3'rd

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const app=express();
 app.listen(8000, ()=>{
     console.log("8000번 서버 접속")
 })
+
+// 기본 주소 요청
+// localhost:8000
+app.get("/", ()=>{
+    console.log("기본주소 요청");
+})
+
+app.get("/hello", ()=>{
+    console.log("hello 주소 요청");
+})
